8b2a636a5cd35aa40260bf4190c51b9b0d967bc8
muhlenXi <xiyinjun_545@163.com>
2019-06-12 23:56:47 +0800

    Add Swift simu duck empty.

---
+++ demos/strategy/SwiftSimuDuck/SimuDuck/SimuDuck/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimuDuck
+//
+//  Created by muhlenXi on 2019/6/12.
+//  Copyright © 2019 muhlenXi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
